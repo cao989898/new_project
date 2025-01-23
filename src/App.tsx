c4d645c10720de84486366534ba27c95c2d87b39
cao989898 <criandoumcao@gmail.com>
2025-01-23 18:01:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HeroSection } from './components/HeroSection';
 import { ServicesSection } from './components/ServicesSection';
@@ -5,20 +6,28 @@ import { AboutSection } from './components/AboutSection';
 import { ProjectsSection } from './components/ProjectsSection';
 import { TestimonialsSection } from './components/TestimonialsSection';
 import { ContactSection } from './components/ContactSection';
+import Appointment from './components/Appointment';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <HeroSection />
-        <ServicesSection />
-        <AboutSection />
-        <ProjectsSection />
-        <TestimonialsSection />
-      </main>
-      <ContactSection />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <main>
+              <HeroSection />
+              <ServicesSection />
+              <AboutSection />
+              <ProjectsSection />
+              <TestimonialsSection />
+            </main>
+          } />
+          <Route path="/appointment" element={<Appointment />} />
+        </Routes>
+        <ContactSection />
+      </div>
+    </Router>
   );
 }
 
