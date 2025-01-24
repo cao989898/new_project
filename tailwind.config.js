b47e430feaa8b9ad1fdfcb4d8c8e27d9caa93bf3
cao989898 <criandoumcao@gmail.com>
2025-01-23 20:32:51 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#FF7315',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
