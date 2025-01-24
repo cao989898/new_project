7565aa3068bae984607e77b5a28a71552c15815d
cao989898 <criandoumcao@gmail.com>
2025-01-23 19:02:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+// File: src/App.tsx
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { HeroSection } from './components/HeroSection';
-import { ServicesSection } from './components/ServicesSection';
-import { AboutSection } from './components/AboutSection';
-import { ProjectsSection } from './components/ProjectsSection';
-import { TestimonialsSection } from './components/TestimonialsSection';
-import { ContactSection } from './components/ContactSection';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import ServicesSection from './components/ServicesSection';
+import AboutSection from './components/AboutSection';
+import ProjectsSection from './components/ProjectsSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import ContactSection from './components/ContactSection';
 import Appointment from './components/Appointment';
 
 function App() {
@@ -14,16 +16,24 @@ function App() {
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/" element={
-            <main>
-              <HeroSection />
-              <ServicesSection />
-              <AboutSection />
-              <ProjectsSection />
-              <TestimonialsSection />
-            </main>
-          } />
+          {/* Home Route */}
+          <Route
+            path="/"
+            element={
+              <main>
+                <HeroSection />
+                <ServicesSection />
+                <AboutSection />
+                <ProjectsSection />
+                <TestimonialsSection />
+              </main>
+            }
+          />
+
+          {/* Appointment Route */}
           <Route path="/appointment" element={<Appointment />} />
+
+          {/* Additional Routes (if needed) */}
         </Routes>
         <ContactSection />
       </div>
